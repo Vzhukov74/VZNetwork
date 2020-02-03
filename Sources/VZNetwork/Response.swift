ae698b128b95c92780d2300cb91a509994458213
vladislav zhukov <vzhukov@protonmail.ch>
2020-02-03 17:51:45 +0500

    fixed Response

--- Sources/VZNetwork/Response.swift
+++ Sources/VZNetwork/Response.swift
@@ -5,10 +5,10 @@ public extension VZNetwork {
         public let response: HTTPURLResponse
         public let data: Data
         
-        var code: Int { return response.statusCode }
-        var isSuccess: Bool { return code == 200 }
-        var etag: String? { return response.allHeaderFields["Etag"] as? String }
-        var description: String? { return try? JSONSerialization.jsonObject(with: data, options: []) as? String }
+        public var code: Int { return response.statusCode }
+        public var isSuccess: Bool { return code == 200 }
+        public var etag: String? { return response.allHeaderFields["Etag"] as? String }
+        public var description: String? { return try? JSONSerialization.jsonObject(with: data, options: []) as? String }
         
         public init?(response: HTTPURLResponse?, data: Data?) {
             guard let response = response, let data = data else { return nil }
